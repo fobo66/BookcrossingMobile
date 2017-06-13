299c963dc1af73ac6a6f7febe2b51f28db3c8d52
Andrey Mukamolow <fobo66@protonmail.com>
2017-06-14 00:37:57 +0300

    Releasing acquired book dialog

--- app/src/main/java/com/bookcrossing/mobile/presenters/BookItemPresenter.java
+++ app/src/main/java/com/bookcrossing/mobile/presenters/BookItemPresenter.java
@@ -1,16 +1,16 @@
 package com.bookcrossing.mobile.presenters;
 
+import android.support.annotation.NonNull;
 import com.arellomobile.mvp.InjectViewState;
 import com.bookcrossing.mobile.ui.bookpreview.BookItemView;
 
-@InjectViewState
-public class BookItemPresenter extends BasePresenter<BookItemView> {
+@InjectViewState public class BookItemPresenter extends BasePresenter<BookItemView> {
 
-    public static final String TAG = "BookItemPresenter";
+  public static final String TAG = "BookItemPresenter";
 
-
-    public void releaseCurrentBook(String key) {
-        acquiredBooks().child(key).removeValue();
-        books().child(key).child("free").setValue(true);
-    }
+  public void releaseCurrentBook(@NonNull String key, @NonNull String position) {
+    acquiredBooks().child(key).removeValue();
+    books().child(key).child("free").setValue(true);
+    books().child(key).child("positionName").setValue(position);
+  }
 }
