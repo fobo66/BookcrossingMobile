4d36f2c0fd829a69301912234d3458f57a6ed870
Andrey Mukamolow <fobo66@protonmail.com>
2017-06-14 20:44:03 +0300

    Reorder commands for releasing acquired books for Cloud Functions convenience

--- app/src/main/java/com/bookcrossing/mobile/presenters/BookItemPresenter.java
+++ app/src/main/java/com/bookcrossing/mobile/presenters/BookItemPresenter.java
@@ -10,7 +10,7 @@ import com.bookcrossing.mobile.ui.bookpreview.BookItemView;
 
   public void releaseCurrentBook(@NonNull String key, @NonNull String position) {
     acquiredBooks().child(key).removeValue();
-    books().child(key).child("free").setValue(true);
     books().child(key).child("positionName").setValue(position);
+    books().child(key).child("free").setValue(true);
   }
 }
