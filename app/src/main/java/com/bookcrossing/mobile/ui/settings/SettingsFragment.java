87a8309030cd6e04255501c0a7f2dfeeae72662e
Andrey Mukamolow <fobo66@protonmail.com>
2017-07-02 20:49:13 +0300

    Settings fragment now can change MainActivity's title

--- app/src/main/java/com/bookcrossing/mobile/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/bookcrossing/mobile/ui/settings/SettingsFragment.java
@@ -1,9 +1,13 @@
 package com.bookcrossing.mobile.ui.settings;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.View;
 import com.bookcrossing.mobile.R;
+import com.bookcrossing.mobile.util.BookListenerDelegate;
 import com.bookcrossing.mobile.util.Constants;
 import com.bookcrossing.mobile.util.PreferenceChangeListener;
 
@@ -14,10 +18,29 @@ import com.bookcrossing.mobile.util.PreferenceChangeListener;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
-  private Preference.OnPreferenceChangeListener listener = new PreferenceChangeListener();
+  private Preference.OnPreferenceChangeListener preferenceChangeListener =
+      new PreferenceChangeListener();
+  private BookListenerDelegate delegate;
+
+  @Override public void onAttach(Context context) {
+    super.onAttach(context);
+
+    delegate = new BookListenerDelegate(context);
+  }
+
+  @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    delegate.setTitle(R.string.action_settings);
+  }
+
+  @Override public void onDetach() {
+    super.onDetach();
+    delegate.detachListener();
+  }
 
   @Override public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
     setPreferencesFromResource(R.xml.pref_main, rootKey);
-    findPreference(Constants.EXTRA_DEFAULT_CITY).setOnPreferenceChangeListener(listener);
+    findPreference(Constants.EXTRA_DEFAULT_CITY).setOnPreferenceChangeListener(
+        preferenceChangeListener);
   }
 }
