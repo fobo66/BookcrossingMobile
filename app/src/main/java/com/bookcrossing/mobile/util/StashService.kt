c31666f7238277b1bce7a831941433727af6e915
Andrey Mukamolov <fobo66@protonmail.com>
2019-08-28 23:16:38 +0300

    Formatting of StashService

--- app/src/main/java/com/bookcrossing/mobile/util/StashService.kt
+++ app/src/main/java/com/bookcrossing/mobile/util/StashService.kt
@@ -18,62 +18,62 @@ import com.google.firebase.messaging.RemoteMessage
 
 class StashService : FirebaseMessagingService() {
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        super.onMessageReceived(remoteMessage)
+  override fun onMessageReceived(remoteMessage: RemoteMessage) {
+    super.onMessageReceived(remoteMessage)
 
-        val intent = Intent(this, BookActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        intent.putExtra(EXTRA_KEY, remoteMessage.data["key"])
+    val intent = Intent(this, BookActivity::class.java)
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+    intent.putExtra(EXTRA_KEY, remoteMessage.data["key"])
 
-        val pendingIntent = PendingIntent.getActivity(this, 123456, intent, PendingIntent.FLAG_ONE_SHOT)
-        val channelName = getString(R.string.stash_notification_channel)
-        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+    val pendingIntent = PendingIntent.getActivity(this, 123456, intent, PendingIntent.FLAG_ONE_SHOT)
+    val channelName = getString(R.string.stash_notification_channel)
+    val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
-        val notificationBuilder = NotificationCompat.Builder(this, channelName)
-                .setSmallIcon(R.drawable.ic_book_black_24dp)
-                .setContentTitle(resolveLocalizedNotificationText(remoteMessage.notification?.titleLocalizationKey))
-                .setContentText(resolveLocalizedNotificationText(remoteMessage.notification?.bodyLocalizationKey))
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent)
+    val notificationBuilder = NotificationCompat.Builder(this, channelName)
+      .setSmallIcon(R.drawable.ic_book_black_24dp)
+      .setContentTitle(resolveLocalizedNotificationText(remoteMessage.notification?.titleLocalizationKey))
+      .setContentText(resolveLocalizedNotificationText(remoteMessage.notification?.bodyLocalizationKey))
+      .setAutoCancel(true)
+      .setSound(defaultSoundUri)
+      .setContentIntent(pendingIntent)
 
-        val notificationChannel = createNotificationChannel(channelName)
+    val notificationChannel = createNotificationChannel(channelName)
 
-        with(NotificationManagerCompat.from(applicationContext)) {
-            if (notificationChannel != null) {
-                createNotificationChannel(notificationChannel)
-            }
-            notify(NOTIFICATION_ID, notificationBuilder.build())
-        }
-
+    with(NotificationManagerCompat.from(applicationContext)) {
+      if (notificationChannel != null) {
+        createNotificationChannel(notificationChannel)
+      }
+      notify(NOTIFICATION_ID, notificationBuilder.build())
     }
 
-    private fun resolveLocalizedNotificationText(localizationKey: String?): CharSequence? {
-        return getString(resources.getIdentifier(localizationKey, "string", packageName))
+  }
+
+  private fun resolveLocalizedNotificationText(localizationKey: String?): CharSequence? {
+    return getString(resources.getIdentifier(localizationKey, "string", packageName))
+  }
+
+  private fun createNotificationChannel(channelName: String): NotificationChannel? {
+    // Create the NotificationChannel, but only on API 26+ because
+    // the NotificationChannel class is new and not in the support library
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+      val descriptionText = getString(R.string.stash_notification_channel_description)
+      val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+      return NotificationChannel(CHANNEL_ID, channelName, importance).apply {
+        description = descriptionText
+        lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+        enableVibration(false)
+        enableLights(false)
+      }
     }
 
-    private fun createNotificationChannel(channelName: String): NotificationChannel? {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+    return null
+  }
 
-            val descriptionText = getString(R.string.stash_notification_channel_description)
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-
-            return NotificationChannel(CHANNEL_ID, channelName, importance).apply {
-                description = descriptionText
-                lockscreenVisibility = Notification.VISIBILITY_PUBLIC
-                enableVibration(false)
-                enableLights(false)
-            }
-        }
-
-        return null
-    }
-
-    companion object {
-        private const val CHANNEL_ID: String = "stash"
-        private const val NOTIFICATION_ID = 261998
-    }
+  companion object {
+    private const val CHANNEL_ID: String = "stash"
+    private const val NOTIFICATION_ID = 261998
+  }
 
 }
