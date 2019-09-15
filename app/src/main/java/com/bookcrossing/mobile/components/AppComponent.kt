279bf5e458b988bf313809f4b1102aa0346d9417
Andrey Mukamolov <fobo66@protonmail.com>
2019-09-15 23:50:53 +0300

    Cleanup

--- app/src/main/java/com/bookcrossing/mobile/components/AppComponent.kt
+++ app/src/main/java/com/bookcrossing/mobile/components/AppComponent.kt
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2019 Andrey Mukamolov
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.bookcrossing.mobile.components
 
 import com.bookcrossing.mobile.modules.ApiModule
@@ -20,27 +36,27 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = [PrefModule::class, LocationModule::class, ApiModule::class, ProvidersModule::class])
 interface AppComponent {
-    @Component.Builder
-    interface Builder {
-        @BindsInstance
-        fun application(application: App): Builder
+  @Component.Builder
+  interface Builder {
+    @BindsInstance
+    fun application(application: App): Builder
 
-        fun prefModule(prefModule: PrefModule): Builder
+    fun prefModule(prefModule: PrefModule): Builder
 
-        fun apiModule(apiModule: ApiModule): Builder
+    fun apiModule(apiModule: ApiModule): Builder
 
-        fun locationModule(locationModule: LocationModule): Builder
+    fun locationModule(locationModule: LocationModule): Builder
 
-        fun build(): AppComponent
-    }
+    fun build(): AppComponent
+  }
 
-    fun inject(app: App)
+  fun inject(app: App)
 
-    fun inject(firebaseWrapper: FirebaseWrapper)
+  fun inject(firebaseWrapper: FirebaseWrapper)
 
-    fun inject(systemServicesWrapper: SystemServicesWrapper)
+  fun inject(systemServicesWrapper: SystemServicesWrapper)
 
-    fun inject(presenter: MainPresenter)
+  fun inject(presenter: MainPresenter)
 
   fun inject(presenter: SearchPresenter)
 }
