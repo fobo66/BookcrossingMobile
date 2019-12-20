fd0e855906566ca340f79e66ecf2d20072f3502b
Andrey Mukamolov <fobo66@protonmail.com>
2019-12-20 23:34:08 +0300

    Initial bottom sheet version

---
+++ app/src/main/java/com/bookcrossing/mobile/ui/map/LocationPicker.kt
@@ -0,0 +1,37 @@
+/*
+ *    Copyright 2019 Andrey Mukamolov
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.bookcrossing.mobile.ui.map
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bookcrossing.mobile.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+/**P
+ * A fragment that shows map for picking book's location as a modal bottom sheet.
+ */
+class LocationPicker : BottomSheetDialogFragment() {
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    return inflater.inflate(R.layout.fragment_location_picker, container, false)
+  }
+}
