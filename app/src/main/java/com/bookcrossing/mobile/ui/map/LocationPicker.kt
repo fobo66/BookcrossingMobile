af26ce4e7eaf0e9340001bbe2f4d19bbf5de28f3
Andrey Mukamolov <amv@bamboogroup.eu>
2019-12-22 13:21:04 +0300

    Fixed showing location

--- app/src/main/java/com/bookcrossing/mobile/ui/map/LocationPicker.kt
+++ app/src/main/java/com/bookcrossing/mobile/ui/map/LocationPicker.kt
@@ -16,18 +16,24 @@
 
 package com.bookcrossing.mobile.ui.map
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresPermission
 import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.Unbinder
 import com.bookcrossing.mobile.R
+import com.bookcrossing.mobile.R.string
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.snackbar.Snackbar
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.location.LocationComponent
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
 import com.mapbox.mapboxsdk.location.LocationComponentOptions
@@ -53,6 +59,7 @@ class LocationPicker : BottomSheetDialogFragment(), PermissionsListener {
     return inflater.inflate(R.layout.fragment_location_picker, container, false)
   }
 
+  @SuppressLint("MissingPermission") // permission is checked in PermissionManager
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     unbinder = ButterKnife.bind(this, view)
@@ -108,6 +115,7 @@ class LocationPicker : BottomSheetDialogFragment(), PermissionsListener {
     permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
   }
 
+  @RequiresPermission(ACCESS_FINE_LOCATION)
   private fun setupCurrentLocation(
     style: Style,
     locationComponent: LocationComponent
@@ -118,22 +126,36 @@ class LocationPicker : BottomSheetDialogFragment(), PermissionsListener {
     val locationComponentActivationOptions = LocationComponentActivationOptions
       .builder(requireContext(), style)
       .locationComponentOptions(locationComponentOptions)
+      .useDefaultLocationEngine(true)
       .build()
 
     locationComponent.activateLocationComponent(locationComponentActivationOptions)
+    locationComponent.isLocationComponentEnabled = true
   }
 
   override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
-    Snackbar.make(mapView, R.string.location_permission_denied_prompt, Snackbar.LENGTH_LONG).show()
+    showExplanation()
   }
 
+  @SuppressLint("MissingPermission") // permission is checked by PermissionManager
   override fun onPermissionResult(granted: Boolean) {
     if (granted) {
       mapView.getMapAsync { map ->
         map.getStyle { style ->
           setupCurrentLocation(style, map.locationComponent)
+
+          map.cameraPosition = CameraPosition.Builder()
+            .target(LatLng(map.locationComponent.lastKnownLocation))
+            .build()
         }
       }
+    } else {
+      showExplanation()
+      dismiss()
     }
+  }
+
+  private fun showExplanation() {
+    Snackbar.make(mapView, string.location_permission_denied_prompt, Snackbar.LENGTH_LONG).show()
   }
 }
