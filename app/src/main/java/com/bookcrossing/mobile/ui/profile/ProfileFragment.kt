dfef61fbf330c0f4b9116e17e21c2c010b57cb89
Andrey Mukamolov <fobo66@protonmail.com>
2020-01-03 01:09:50 +0300

    Temporarily removed empty list indicator
    Turns out it doesn't work :(

--- app/src/main/java/com/bookcrossing/mobile/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/bookcrossing/mobile/ui/profile/ProfileFragment.kt
@@ -1,17 +1,17 @@
 /*
- *     Copyright 2019 Andrey Mukamolov
+ *    Copyright 2019 Andrey Mukamolov
  *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 package com.bookcrossing.mobile.ui.profile
 
@@ -20,7 +20,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.appcompat.widget.AppCompatTextView
@@ -91,10 +90,6 @@ class ProfileFragment : BaseFragment(), ProfileView {
       .placeholder(drawable.ic_account_circle_black_24dp)
       .transition(DrawableTransitionOptions.withCrossFade())
       .into(profileImage)
-
-    if (adapter.itemCount == 0) {
-      acquiredBooksListEmptyIndicator.visibility = VISIBLE
-    }
   }
 
   private fun setupAcquiredBookList() {
