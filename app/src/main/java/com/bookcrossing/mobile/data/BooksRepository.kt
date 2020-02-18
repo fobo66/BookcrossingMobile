1c4a8cf58cd2e8dcfa99e7116ec22718dbb81c78
Andrey Mukamolov <fobo66@protonmail.com>
2020-02-18 22:37:50 +0300

    Added books repo

---
+++ app/src/main/java/com/bookcrossing/mobile/data/BooksRepository.kt
@@ -0,0 +1,61 @@
+/*
+ *    Copyright 2019 Andrey Mukamolov
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.bookcrossing.mobile.data
+
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import javax.inject.Inject
+
+/** Wrapper around Firebase database references */
+class BooksRepository @Inject constructor(
+  private val database: FirebaseDatabase
+) {
+
+  /** Load all books */
+  fun books(): DatabaseReference {
+    return database.getReference("books")
+  }
+
+  /** Load books stashed by the user */
+  fun stash(userId: String): DatabaseReference {
+    return database.getReference("stash")
+      .child(userId)
+  }
+
+  /** Load books acquired by the user */
+  fun acquiredBooks(userId: String): DatabaseReference {
+    return database.getReference("acquiredBooks")
+      .child(userId)
+  }
+
+  /** Load all books' current positions */
+  fun places(): DatabaseReference {
+    return database.getReference("places")
+  }
+
+  /** Load given book's current position */
+  fun place(key: String): DatabaseReference {
+    return database.getReference("places")
+      .child(key)
+  }
+
+  /** Load given book's positions history */
+  fun placesHistory(key: String): DatabaseReference {
+    return database.getReference("placesHistory")
+      .child(key)
+  }
+}
