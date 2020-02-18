86a112f3ae93b9ec957251cdc1236260bd91da40
Andrey Mukamolov <fobo66@protonmail.com>
2020-02-18 22:34:09 +0300

    Added auth repo

---
+++ app/src/main/java/com/bookcrossing/mobile/data/AuthRepository.kt
@@ -0,0 +1,48 @@
+/*
+ *    Copyright 2019 Andrey Mukamolov
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.bookcrossing.mobile.data
+
+import com.bookcrossing.mobile.util.DEFAULT_USER
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuth.AuthStateListener
+import io.reactivex.Observable
+import javax.inject.Inject
+
+/** Repository to handle auth */
+class AuthRepository @Inject constructor(
+  private val auth: FirebaseAuth
+) {
+
+  /** Id of the currently signed in user */
+  val userId: String
+    get() = auth.currentUser?.uid ?: DEFAULT_USER
+
+  /** Check if user is authenticated */
+  val isAuthenticated: Boolean
+    get() = auth.currentUser != null
+
+  /** Observe auth state changes */
+  fun onAuthenticated() = Observable.create<FirebaseAuth> { emitter ->
+    val authStateListener = AuthStateListener { firebaseAuth ->
+      if (!emitter.isDisposed) {
+        emitter.onNext(firebaseAuth)
+      }
+    }
+    auth.addAuthStateListener(authStateListener)
+    emitter.setCancellable { auth.removeAuthStateListener(authStateListener) }
+  }
+}
