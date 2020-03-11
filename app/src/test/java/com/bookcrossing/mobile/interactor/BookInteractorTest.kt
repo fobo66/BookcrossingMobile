d735420383b401af2f2749afbbbd28bd06e6268d
Andrey Mukamolov <amv@bamboogroup.eu>
2020-03-11 13:02:15 +0300

    Replaced tests for interactor with mocked unit tests

--- app/src/test/java/com/bookcrossing/mobile/interactor/BookInteractorTest.kt
+++ app/src/test/java/com/bookcrossing/mobile/interactor/BookInteractorTest.kt
@@ -16,15 +16,11 @@
 
 package com.bookcrossing.mobile.interactor
 
-import androidx.test.platform.app.InstrumentationRegistry
 import com.bookcrossing.mobile.data.AuthRepository
 import com.bookcrossing.mobile.data.BooksRepository
 import com.bookcrossing.mobile.models.Book
 import com.bookcrossing.mobile.models.Coordinates
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
+import io.mockk.mockk
 import io.reactivex.schedulers.Schedulers
 import org.junit.Before
 import org.junit.Test
@@ -35,21 +31,8 @@ class BookInteractorTest {
 
   @Before
   fun setUp() {
-    val firebaseApp = FirebaseApp.initializeApp(
-      InstrumentationRegistry.getInstrumentation().targetContext,
-      FirebaseOptions.Builder()
-        .setApplicationId("test")
-        .setApiKey("apikey")
-        .setDatabaseUrl("http://tests.fblocal.com:9000")
-        .build(),
-      "testapp" + Math.random()
-    )
-
-    val database = FirebaseDatabase.getInstance(firebaseApp)
-    val auth = FirebaseAuth.getInstance(firebaseApp)
-
-    val authRepository = AuthRepository(auth)
-    val booksRepository = BooksRepository(database)
+    val authRepository = mockk<AuthRepository>()
+    val booksRepository = mockk<BooksRepository>()
 
     bookInteractor = BookInteractor(booksRepository, authRepository)
   }
