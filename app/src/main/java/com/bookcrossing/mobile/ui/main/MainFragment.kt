135a685c38b3cc60988ccb55c4a6bec273f0196b
Andrey Mukamolov <amv@bamboogroup.eu>
2020-05-21 15:15:02 +0300

    Remove adapter field

--- app/src/main/java/com/bookcrossing/mobile/ui/main/MainFragment.kt
+++ app/src/main/java/com/bookcrossing/mobile/ui/main/MainFragment.kt
@@ -37,10 +37,8 @@ import com.bookcrossing.mobile.presenters.MainPresenter
 import com.bookcrossing.mobile.ui.base.BaseFragment
 import com.bookcrossing.mobile.util.RC_SIGN_IN
 import com.bookcrossing.mobile.util.adapters.BooksAdapter
-import com.bookcrossing.mobile.util.adapters.BooksViewHolder
 import com.firebase.ui.auth.ErrorCodes
 import com.firebase.ui.auth.IdpResponse
-import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions.Builder
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
@@ -68,8 +66,6 @@ class MainFragment : BaseFragment(), MainView {
   lateinit var presenterProvider: Provider<MainPresenter>
 
   private val presenter: MainPresenter by moxyPresenter { presenterProvider.get() }
-
-  private lateinit var adapter: FirebaseRecyclerAdapter<Book, BooksViewHolder>
 
   override fun onAttach(context: Context) {
     injector.inject(this)
@@ -156,14 +152,13 @@ class MainFragment : BaseFragment(), MainView {
 
   private fun setupBookList() {
     rv.layoutManager = LinearLayoutManager(requireContext())
-    adapter = BooksAdapter(
+
+    rv.adapter = BooksAdapter(
       presenter.bookCoverResolver,
       Builder<Book>().setQuery(presenter.books, Book::class.java)
         .setLifecycleOwner(viewLifecycleOwner)
         .build()
     )
-
-    rv.adapter = adapter
   }
 
   override fun onDestroyView() {
